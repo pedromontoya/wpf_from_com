3fb3bea3f77dac501c159a54e845d3080b23902f
unknown <Pedro.Montoya@PMONTOYA02.corp.neudesic.net>
2013-04-09 22:04:25 -0700

    updated com project, adding logic to show window and resolve dependencies

--- COM.Sample/SampleWpfInvoker.cs
+++ COM.Sample/SampleWpfInvoker.cs
@@ -17,29 +17,42 @@ namespace COM
     {
         public SampleWpfInvoker()
         {
+            //The executing directory used to resolve dependent assemblies in the current appDomain
             var executingDir = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            //The directory where this assembly is located
             var asseblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             if (!executingDir.Equals(asseblyDir, StringComparison.InvariantCultureIgnoreCase))
             {
-                AppDomain.CurrentDomain.AssemblyResolve += delegate(object obj, ResolveEventArgs e)
+                AppDomain.CurrentDomain.AssemblyResolve += (object obj, ResolveEventArgs args) =>
                 {
+                    var currentAssembly = Assembly.GetExecutingAssembly();
+                    var assemblyReferences = currentAssembly.GetReferencedAssemblies();
 
+                    var needeAssebly = assemblyReferences.Where(x => x.Name == args.Name).FirstOrDefault();
+                    if (needeAssebly == null)
+                        return null;
+
+                    //Check the assembly directory for dependencies
+                    return Assembly.LoadFrom(asseblyDir + @"\" + args.Name +".dll");
                 };
             }
         }
-        //mainDir = appdomain.currentDom.settupInfo.appbase
-        //dotNetPath = path.getDirName(Assembly.GetExecutingAssem().Location)
 
-        //if(!mainDir.equal(dotNetPath)
-        //Appdomain.currentasembly.resolve +=
         public void ShowWindow()
         {
-            var application = new Application();
-            var mainWindow = new MainWindow();
-            application.MainWindow = mainWindow;
-
-            application.MainWindow.ShowDialog();
+            try
+            {
+                var application = new Application();
+                var mainWindow = new MainWindow();
+                application.MainWindow = mainWindow;
+                application.MainWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                //Handle uncaught exceptions that may have happened in the WPF application. 
+            }
         }
     }
 }
